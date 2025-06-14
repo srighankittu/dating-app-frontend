a8764008b8a11a4ee135bd23a73f65a2f2947f41
srighankittu <107845663+srighankittu@users.noreply.github.com>
2025-06-14 16:33:30 +0530

    Added Routing and Basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
