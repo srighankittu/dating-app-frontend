78eb742069f3caf71372ec36ca9149715ce9e892
srighankittu <107845663+srighankittu@users.noreply.github.com>
2025-06-15 21:56:58 +0530

    removed unused var

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const { initiateLogin, user, isLoading, error } = useLogin({
+  const { initiateLogin, isLoading, error } = useLogin({
     email,
     password,
   });
